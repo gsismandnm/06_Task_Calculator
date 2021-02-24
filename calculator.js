f4413c2097064c39d44b77bc1f80d6a5884c98ea
Gökan ŞİŞMAN <gknsismandenmark@gmail.com>
2021-02-24 20:33:02 +0200

    update

---
+++ calculator.js
@@ -0,0 +1,18 @@
+
+//islem isaretini lutfen string deger olarak girelim!!
+
+function hesapMakinesi (sayi1, sayi2){
+    
+ //   if( isNaN(sayi1) || isNaN(sayi2 ))console.log('Lütfen bir rakam giriniz!!!');
+        
+function toplamaIslemi ({result: sayi1 + sayi2});
+
+function cıkarmaIslemi({result:sayi1 - sayi2});
+
+function carpmaIslemi ({result:sayi1 * sayi2});
+ 
+function bölmeIslemi ({ result:sayı1/sayı2});
+ 
+ 
+    };
+    
