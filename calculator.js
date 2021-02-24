341b4a3dbfdf52d2d5a35b2279071aa3ade44b91
Gökan ŞİŞMAN <gknsismandenmark@gmail.com>
2021-02-24 21:19:05 +0200

    Update 5

--- calculator.js
+++ calculator.js
@@ -1,18 +1,61 @@
 
 //islem isaretini lutfen string deger olarak girelim!!
 
-function hesapMakinesi (sayi1, sayi2){
+function hesapMakinesi(sayi1, sayi2, islemIsareti){
+
     
- //   if( isNaN(sayi1) || isNaN(sayi2 ))console.log('Lütfen bir rakam giriniz!!!');
+    if( isNaN(sayi1) || isNaN(sayi2 )) 
+    console.log('Lütfen bir sayı giriniz!!');
+    else{
+        if ( islemIsareti == '+'){
+            let sonuc = {
+              islemSonucu:  sayi1+sayi2,
+              secim: 'toplama' 
+    
+                     }
+               return sonuc;
+            } 
         
-function toplamaIslemi ({result: sayi1 + sayi2});
+        else if ( islemIsareti == '-'){
+            let sonuc = {
+              islemSonucu:  sayi1-sayi2,
+              secim: 'cikarma'
+    
+                     }
+                return sonuc;
+            }
+        
+        else if ( islemIsareti == '*'){
+            let sonuc = {
+              islemSonucu:  sayi1*sayi2,
+              secim: 'carpma'
+    
+                     }
+                 return sonuc;
+            }
+        
+        else if ( islemisareti == '/'){
+       
+                 if(sayi2==0) console.log('ikinci sayi sifira esit.Bu yuzden sonuc belirsizdir');
+                 else {
+                    let sonuc = {
+                      islemsonucu:  sayi1/sayi2,
+                      secim: 'bolme'
+            
+                             }
+                 return sonuc;
+            }
+          }
+    
+        else console.log('boyle bir islem bulunamadi!!');
+    }
+    
+    }
+    const islemler = hesapMakinesi("[]", "[]", "[]");
+        
+    console.log(islemler);
+    //input = sayi1();
+    //input = sayi2();
+    //input = islemIsareti();
 
-function cıkarmaIslemi({result:sayi1 - sayi2});
-
-function carpmaIslemi ({result:sayi1 * sayi2});
- 
-function bölmeIslemi ({ result:sayı1/sayı2});
- 
- 
-    };
     
